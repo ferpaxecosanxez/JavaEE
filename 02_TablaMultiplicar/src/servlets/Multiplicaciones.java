f84baa5856796c8cc7c28e04f08e112f5e2e3694
Fernando Ismael Pacheco Sánchez <fi-ps@hotmail.com>
2019-03-18 13:55:17 +0100

    Actualización de descripción. Agregamos indentación en el código html que escupe, esta acción se realiza para cuando hacemos Ctrl + U en el navegador, este nos muestre un código legible y ordenado.

--- 02_TablaMultiplicar/src/servlets/Multiplicaciones.java
+++ 02_TablaMultiplicar/src/servlets/Multiplicaciones.java
@@ -10,12 +10,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet que genera código html y su contenido es una tabla que contiene las
- * multiplicaciones del 1 al 10.
+ * Servlet simple que genera y escupe código html. El contenido html es una
+ * tabla que contiene las multiplicaciones del 1 al 10.
  * <p>
- * Para ejecutar la aplicación debe ejecutarse directamente el servlet, porque
- * el proyecto web no contiene el fichero web.xml, el cual no es necesario para
- * este ejercicio.
+ * Para ver el resultado debemos ejecutar el servlet de forma directa en Tomcat
+ * ya que el proyecto web no contiene el fichero web.xml.
+ * <p>
+ * Agregamos indentación en el código html que escupe, esta acción se realiza
+ * para cuando hacemos Ctrl + U en el navegador, este nos muestre un código
+ * legible y ordenado.
  * 
  * @author fips
  *
@@ -33,19 +36,32 @@ public class Multiplicaciones extends HttpServlet {
 		// Try con recursos.
 		try (PrintWriter out = response.getWriter()) {
 			out.println("<!DOCTYPE html>");
-			out.println("<html lang=\"es\">");
+			out.println("<html lang='es'>");
 			out.println("<body>");
-			out.println("<table border='1'>");
+			out.println("");// lïnea en blanco.
+			out.println("\t<table border='1'>");
 			for (int i = 1; i <= 10; i++) {
-				out.println("<tr>");
+				out.println("\t\t<tr>");
 				for (int j = 1; j <= 10; j++) {
-					out.println("<td>");
-					out.println(i * j);
-					out.println("</td>");
+					// Si es el primer dato que se pinta, este debe estar indentado.
+					if (j == 1) {
+						out.print("\t\t\t");
+					} else {
+						// Lo pintanmos junto al dato anterior.
+						out.print("<td>");
+						out.print(i * j);
+						out.print("</td>");
+					}
+					
+					// Si es el último dato que pintamos, este debe tener un salto de línea.
+					if (j == 10) {
+						out.println("");
+					}
 				}
-				out.println("</tr>");
+				out.println("\t\t</tr>");
 			}
-			out.println("</table>");
+			out.println("\t</table>");
+			out.println("");// lïnea en blanco.
 			out.println("</body>");
 			out.println("</html>");
 		} // Fin de try
